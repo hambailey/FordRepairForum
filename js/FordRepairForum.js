36633f44c3b4e5550e5f19332f7474ab7f4a6505
HamBailey <YOUR_EMAIL_ADDRESS_HERE@users.noreply.github.com>
2020-08-24 23:10:26 -0400

    Added form button color transition

--- js/FordRepairForum.js
+++ js/FordRepairForum.js
@@ -1,2 +1,14 @@
 // FordRepairForum.js
 
+// login form button fades colors on hover and pointer removal.
+
+$(document).ready(function(){
+	"use strict";
+	
+	var toggleButton = $("#formButton");
+	toggleButton.hover(function(){
+		formButton.css("background-color", "grey");
+	}, function(){
+		formButton.css("background-color", "smokewhite");
+	});
+})
